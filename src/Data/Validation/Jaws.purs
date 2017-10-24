1d921963825b4ae5ccb03b6237fda2c9e4228840
Tomasz Rybarczyk <paluho@gmail.com>
2017-10-24 19:52:38 +0200

    Refactor whole stack and add basic combinators

--- src/Data/Validation/Jaws.purs
+++ src/Data/Validation/Jaws.purs
@@ -2,28 +2,88 @@ module Data.Validation.Jaws where
 
 import Prelude
 
-import Data.Either (Either, Either(..), isLeft)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.Except.Trans (ExceptT(..))
+import Data.Array (catMaybes, head, uncons)
+import Data.Bifunctor (class Bifunctor)
+import Data.Either (Either(Left, Right), hush, note)
+import Data.EitherR (fmapLT)
 import Data.Int (fromString)
-import Data.Maybe (Maybe(..), fromMaybe, maybe)
-import Data.Newtype (class Newtype)
+import Data.Maybe (Maybe(Just, Nothing), fromMaybe)
+import Data.NonEmpty (NonEmpty, (:|))
+import Data.Profunctor (class Profunctor, dimap)
+import Data.Profunctor.Choice (class Choice, right)
 import Data.Record (get, insert)
-import Data.StrMap (StrMap, empty, fromFoldable, lookup)
+import Data.Record.Builder (build, merge)
+import Data.StrMap (StrMap, lookup)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
-import Data.Variant (inj, Variant)
+import Data.Variant (Variant, inj)
 import Type.Row (class RowLacks)
 
-newtype Validation tok a b = Validation (tok → (a → b))
+-- | Don't stare to long at this type it's really general...
+-- | Check `Validation` and `ValidationToRecord`
+newtype ValidationM m tok a b = ValidationM (tok → a → m b)
 
-instance semigroupoidValidation ∷ Semigroupoid (Validation tok) where
-  compose (Validation v1) (Validation v2) =
-    Validation (\tok →
-      let
-        f1 = v1 tok
-        f2 = v2 tok
-      in (f1 <<< f2))
+instance semigroupoidValidationM ∷ (Bind m) ⇒ Semigroupoid (ValidationM m tok) where
+  compose (ValidationM v2) (ValidationM v1) =
+    ValidationM (\tok → v1 tok >=> v2 tok)
 
-instance categoryValidation ∷ Category (Validation tok) where
-  id = Validation (const id)
+instance categoryValidation ∷ (Monad m) ⇒ Category (ValidationM m tok) where
+  id = ValidationM (const pure)
+
+instance profunctorValidation ∷ (Monad m) ⇒ Profunctor (ValidationM m tok) where
+  dimap f g (ValidationM v) =
+    ValidationM (\tok → (f >>> pure) >=> v tok >=> (g >>> pure))
+
+instance choiceValidation ∷ (Monad m) ⇒ Choice (ValidationM m tok) where
+  left (ValidationM v) =
+    ValidationM v'
+   where
+    v' tok (Left input) = Left <$> (v tok input)
+    v' tok (Right r) = pure (Right r)
+  right (ValidationM v) =
+    ValidationM v'
+   where
+    v' tok (Left l) = pure (Left l)
+    v' tok (Right input) = Right <$> (v tok input)
+
+runValidationM ∷ ∀ a b m tok. ValidationM m tok a b → tok → a → m b
+runValidationM (ValidationM v) = v
+
+type Validation m tok a e b = ValidationM (ExceptT e m) tok a b
+
+-- | This is "pure" (in `Reader tok` sens) validation - isomorphic to:
+-- | a → m (Either e b)
+type PureValidation a e b = ∀ m tok. (Monad m) ⇒ Validation m tok a e b
+
+tag :: forall a b e m p r r' tok
+  . RowCons p e r r'
+  ⇒ Monad m
+  ⇒ IsSymbol p
+  ⇒ SProxy p
+  → Validation m tok a e b
+  → Validation m tok a (Variant r') b
+tag p (ValidationM v) =
+  ValidationM (\tok result → fmapLT (inj p) (v tok result))
+
+
+-- This is just (Either i (Tuple i v))
+-- but this alias gave me nearly nothing
+-- in this context so I made brand new type
+data ResultWithFailover i v =
+  Invalid i | Valid i v
+
+instance functorResultWithFailover ∷ Functor (ResultWithFailover i) where
+  map f (Invalid i) = Invalid i
+  map f (Valid i v) = Valid i (f v)
+
+mapI ∷ ∀ i i' v. (i → i') → ResultWithFailover i v → ResultWithFailover i' v
+mapI f (Invalid i) = Invalid (f i)
+mapI f (Valid i v) = Valid (f i) v
+
+instance bifunctorResultWithFailover ∷ Bifunctor ResultWithFailover where
+  bimap g f r = map f (mapI g r)
+
 
 -- | Aggregate validation results.
 -- |
@@ -31,115 +91,162 @@ instance categoryValidation ∷ Category (Validation tok) where
 -- | it passes two types of result:
 -- |
 -- | * just `unwraped` resulting value
+-- |
 -- | * valid value wrapped in `Left` so it can
 -- |   be easily turned into invalid value
 -- |
 -- | i, i' - invalid result types
 -- | v, v' - valid result types
-type TrackedValidation tok i i' v v' =
-  Validation
+type ValidationWithFailover m tok i i' v v' =
+  ValidationM
+    m
     tok
-    (Either (Record i) { v ∷ Record v, i ∷ Record i})
-    (Either (Record i') { v ∷ Record v', i ∷ Record i'})
+    (ResultWithFailover i v)
+    (ResultWithFailover i' v')
 
-run ∷ ∀ i tok v. TrackedValidation tok () i () v → tok → Either (Record i) (Record v)
-run (Validation f) tok =
-  case f tok (Right {v: {}, i: {}}) of
-    Right { v, i } → Right v
-    Left i → Left i
+-- | XXX: migrate to Data.Record.Builder
+type ValidationToRecord m tok i i' v v' =
+  ValidationWithFailover m tok (Record i) (Record i') (Record v) (Record v')
 
--- | Adds validation step and inserts the result
--- | into final record
-addValidationStep ∷ ∀ e a s ir ir' vr vr' tok
-  . (IsSymbol s)
-  ⇒ RowLacks s ir
-  ⇒ RowCons s (Either e a) ir ir'
-  ⇒ RowLacks s vr
-  ⇒ RowCons s a vr vr'
-  ⇒ SProxy s
-  → (tok → Either e a)
-  → TrackedValidation tok ir ir' vr vr'
-addValidationStep p v = Validation (\tok result →
-  case v tok, result of
-    Left i, Left ir → Left (insert p (Left i) ir)
-    Left i, Right {i: ir} → Left (insert p (Left i) ir)
-    Right v, Left ir → Left (insert p (Right v) ir)
-    Right v, Right {v: vr, i: ir} →
-      Right
-        { v: insert p v vr
-        , i: insert p (Right v) ir })
+toValidation ∷ ∀ i m tok v. (Functor m) ⇒ ValidationToRecord m tok () i () v → Validation m tok Unit (Record i) (Record v)
+toValidation (ValidationM f) =
+  ValidationM (\tok _ → ExceptT $ toEither <$> (f tok (Valid {} {})))
+ where
+  toEither = case _  of
+    Invalid i → Left i
+    Valid i v → Right v
 
+tagValidationWithFailover :: forall i i' r r' m p tok v v'
+  . RowCons p i' r r'
+  ⇒ (Functor m)
+  ⇒ IsSymbol p
+  ⇒ SProxy p
+  → ValidationWithFailover m tok i i' v v'
+  → ValidationWithFailover m tok i (Variant r') v v'
+tagValidationWithFailover p (ValidationM v) =
+  ValidationM (\tok result → mapI (inj p) <$> (v tok result))
+
+combine :: forall i i1 i2 i3 m tok v v1 v2 v3
+  . Union i1 i2 i3
+  ⇒ Union v1 v2 v3
+  ⇒ (Apply m)
+  ⇒ ValidationToRecord m tok i i1 v v1
+  → ValidationToRecord m tok i i2 v v2
+  → ValidationToRecord m tok i i3 v v3
+combine (ValidationM v1) (ValidationM v2) =
+  ValidationM (\tok a -> combine' <$> (v1 tok a) <*> (v2 tok a))
+ where
+  combine' r1 r2 = case r1, r2 of
+    Invalid i1, Invalid i2 → Invalid (build (merge i2) i1)
+    Invalid i1, Valid i2 _ → Invalid (build (merge i2) i1)
+    Valid i1 _, Invalid i2 → Invalid (build (merge i2) i1)
+    Valid i1 u1, Valid i2 u2 → Valid (build (merge i2) i1) (build (merge u2) u1)
+
+toRecordField ∷ ∀ b e i i' m p tok v v'
+  . (IsSymbol p)
+  ⇒ (RowCons p (Either e b) i i')
+  ⇒ (RowCons p b v v')
+  ⇒ (RowLacks p i)
+  ⇒ (RowLacks p v)
+  ⇒ (Functor m)
+  ⇒ SProxy p
+  → Validation m tok Unit e b
+  → ValidationToRecord m tok i i' v v'
+toRecordField p (ValidationM f) =
+  ValidationM (\tok r → asRecord r <$> runExceptT (f tok unit))
+ where
+  asRecord (Valid ir vr) r@(Right v) = Valid (insert p r ir) (insert p v vr)
+  asRecord (Valid ir _) e = Invalid (insert p e ir)
+  asRecord (Invalid ir) e = Invalid (insert p e ir)
 
 -- | In http query every key can be repeated
 -- | and can have form:
 -- | ?key ~ (Nothing) or ?key= ~ (Just "") or ?key=v1 ~ (Just "v1")
-type FieldValue = Array (Maybe String)
+type QueryField = Array (Maybe String)
 type Query = StrMap (Array (Maybe String))
 
-queryField ∷ ∀ e a s ir ir' vr vr' tok
+fieldFromRecordField ∷ ∀ a b e ir ir' l m r r' vr vr'
+  . (IsSymbol l)
+  ⇒ (Functor m)
+  ⇒ RowLacks l ir
+  ⇒ RowCons l (Either e b) ir ir'
+  ⇒ RowLacks l vr
+  ⇒ RowCons l a r r'
+  ⇒ RowCons l b vr vr'
+  ⇒ SProxy l
+  → Validation m a Unit e b
+  → ValidationToRecord m (Record r') ir ir' vr vr'
+fieldFromRecordField p (ValidationM v) =
+  toRecordField p (ValidationM (\record _ → v (get p record) unit))
+
+fieldFromRecord ∷ ∀ a b e ir ir' l m r r' vr vr'
+  . (IsSymbol l)
+  ⇒ (Functor m)
+  ⇒ RowLacks l ir
+  ⇒ RowCons l (Either e b) ir ir'
+  ⇒ RowLacks l vr
+  ⇒ RowCons l a r r'
+  ⇒ RowCons l b vr vr'
+  ⇒ SProxy l
+  → Validation m (Record r') Unit e b
+  → ValidationToRecord m (Record r') ir ir' vr vr'
+fieldFromRecord p v =
+  toRecordField p v
+
+fieldFromQuery ∷ ∀ a e m s ir ir' vr vr'
   . (IsSymbol s)
+  ⇒ (Functor m)
   ⇒ RowLacks s ir
   ⇒ RowCons s (Either e a) ir ir'
   ⇒ RowLacks s vr
   ⇒ RowCons s a vr vr'
   ⇒ SProxy s
-  → (Array (Maybe String) → Either e a)
-  → TrackedValidation Query ir ir' vr vr'
-queryField p v =
-  addValidationStep p (\query → v (fromMaybe [] (lookup (reflectSymbol p) query)))
+  → Validation m QueryField Unit e a
+  → ValidationToRecord m Query ir ir' vr vr'
+fieldFromQuery p (ValidationM v) =
+  toRecordField p (ValidationM (\query _ → v (fromMaybe [] (lookup (reflectSymbol p) query)) unit))
 
-missingErr ∷ ∀ v. Variant (missing ∷ Unit | v)
-missingErr = inj (SProxy ∷ SProxy "missing") unit
+-- pureV2R f = ValidationM
 
-emptyErr ∷ ∀ v. Variant (empty ∷ Unit | v)
-emptyErr = inj (SProxy ∷ SProxy "empty") unit
+-- lift function which takes token and result
+pureV ∷ ∀ a b e m tok. (Monad m) ⇒ (tok → a → Either e b) → Validation m tok a e b
+pureV f = ValidationM (\tok → f tok >>> pure >>> ExceptT)
 
-repeatedErr ∷ ∀ v. Variant (repeated ∷ Unit | v)
-repeatedErr = inj (SProxy ∷ SProxy "repeated") unit
+-- lift function which cares only about already processed value
+pureV' ∷ ∀ a b e. (a → Either e b) → PureValidation a e b
+pureV' f = ValidationM (\_ → f >>> pure >>> ExceptT)
 
-present ∷ ∀ v. FieldValue → Either (Variant (missing ∷ Unit, repeated ∷ Unit | v)) (Maybe String)
-present [] = Left missingErr
-present [v] = Right v
-present _ = Left repeatedErr
+catMaybesV :: forall a e. PureValidation (Array (Maybe a)) e (Array a)
+catMaybesV = pureV' (catMaybes >>> Right)
 
-nonEmptyStr ∷ ∀ v. FieldValue → Either (Variant (missing ∷ Unit, repeated ∷ Unit, empty ∷ Unit | v)) String
-nonEmptyStr = present >=> case _ of
-    Nothing → Left emptyErr
-    Just "" → Left emptyErr
-    Just v → Right v
+nonEmpty ∷ ∀ a. PureValidation (Array a) Unit (NonEmpty Array a)
+nonEmpty  =
+  pureV' $ \a → case uncons a of
+    Nothing → Left unit
+    Just { head, tail } → Right (head :| tail)
 
-optional ∷ ∀ a v. (String → Either (Variant (repeated ∷ Unit | v)) a) → (FieldValue → Either (Variant (repeated ∷ Unit | v)) (Maybe a))
-optional v i = do
-  mr ← optional' i
-  case mr of
-    Nothing → pure Nothing
-    Just r → Just <$> v r
+nonEmpty' ∷ ∀ a v. PureValidation (Array a) (Variant (nonEmpty ∷ Unit | v)) (NonEmpty Array a)
+nonEmpty' = tag (SProxy ∷ SProxy "nonEmpty") nonEmpty
+
+scalar ∷ ∀ a. PureValidation (NonEmpty Array a) Unit a
+scalar = pureV' s
  where
-  optional' [] = Right Nothing
-  optional' [Just s] = Right (Just s)
-  optional' [Nothing] = Right Nothing
-  optional' _ = Left repeatedErr
+  s (a :| []) = Right a
+  s _ = Left unit
 
+scalar' ∷ ∀ a v. PureValidation (NonEmpty Array a) (Variant (scalar ∷ Unit | v)) a
+scalar' = tag (SProxy ∷ SProxy "scalar") scalar
 
-notNumberErr ∷ ∀ v. Variant (notNumber ∷ Unit | v)
-notNumberErr = inj (SProxy ∷ SProxy "notNumber") unit
+optional ∷ ∀ a b e m tok. (Monad m) ⇒ Validation m tok a e b → Validation m tok (Array a) e (Maybe b)
+optional v =
+  dimap (head >>> note unit) hush (right v)
 
-int ∷ ∀ v. String → Either (Variant (notNumber ∷ Unit | v)) Int
-int s =
-  case fromString s of
-    Just i → Right i
-    Nothing → Left notNumberErr
+int :: PureValidation String Unit Int
+int = pureV' (fromString >>> note unit)
 
-recordField ∷ ∀ e a s ir ir' vr vr' f r r' tok
-  . (IsSymbol s)
-  ⇒ RowLacks s ir
-  ⇒ RowCons s (Either e a) ir ir'
-  ⇒ RowLacks s vr
-  ⇒ RowCons s a vr vr'
-  ⇒ RowCons s f r r'
-  ⇒ SProxy s
-  → (f → Either e a)
-  → TrackedValidation (Record r') ir ir' vr vr'
-recordField p v =
-  addValidationStep p (\record → v (get p record))
+int' ∷ PureValidation String (Variant (int ∷ Unit)) Int
+int' = tag (SProxy ∷ SProxy "int") int
+
+tokV ∷ ∀ a e i m tok. (Monad m) ⇒ Validation m tok Unit e tok
+tokV = pureV $ (\tok _ → Right tok)
 
